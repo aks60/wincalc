107099bcab2f0e2144e720b6b05406b743271cb4
aksenov <gonved60@gmail.com>
2021-12-23 22:40:33 +0300

    Revert ""
    This reverts commit 2253197c37733b7df3efce27de45dc320b29a946.

--- src/enums/ParamJson.java
+++ src/enums/ParamJson.java
@@ -1,8 +1,18 @@
-select params_id, text from elempar1 where params_id in (1005, 1006, 2005,2006, 3005, 4005, 11005, 12005, 31050, 33071, 34071) union
-select params_id, text from elempar2 where params_id in (1005, 1006, 2005,2006, 3005, 4005, 11005, 12005, 31050, 33071, 34071) union
-select params_id, text from furnpar1 where params_id in (1005, 1006, 2005,2006, 3005, 4005, 11005, 12005, 31050, 33071, 34071) union
-select params_id, text from furnpar2 where params_id in (1005, 1006, 2005,2006, 3005, 4005, 11005, 12005, 31050, 33071, 34071) union
-select params_id, text from glaspar1 where params_id in (1005, 1006, 2005,2006, 3005, 4005, 11005, 12005, 31050, 33071, 34071) union
-select params_id, text from glaspar2 where params_id in (1005, 1006, 2005,2006, 3005, 4005, 11005, 12005, 31050, 33071, 34071) union
-select params_id, text from joinpar1 where params_id in (1005, 1006, 2005,2006, 3005, 4005, 11005, 12005, 31050, 33071, 34071) union
-select params_id, text from joinpar2 where params_id in (1005, 1006, 2005,2006, 3005, 4005, 11005, 12005, 31050, 33071, 34071) order by 1
+package enums;
+
+public enum ParamJson {
+
+    //paramJson("Уник. идентиф."),
+    funic("Уник. идентиф."),
+    typeOpen("Тип открывания"),
+    colorHandl("Цвет ручки створки"),
+    pro4Params("Параметры iokna"),
+    pro4Params2("Параметры iokna"),
+    nunic_iwin("Уникальный идентификатор артикула");
+    String name = "";
+
+    ParamJson(String name) {
+        this.name = name;
+    }
+
+}
